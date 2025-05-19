d592fe9e9ca090a4540a773c5fcd420d8b20713d
AngelDevl <noamharel02@gmail.com>
2025-05-20 02:21:40 +0300

    Updated deploy config 3 | and folders

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,6 @@ export default {
     const url = new URL(request.url);
     const path = url.pathname;
 
-    // Define route mappings
     const routes = {
       "/": "game.html",
       "/game": "game.html",
@@ -11,14 +10,11 @@ export default {
       "/terms-of-service": "terms_of_service.html"
     };
 
-    // Resolve static file path
     const fileToServe = routes[path] || "not_found.html";
-
-    const assetRequest = new Request(new URL(`/${fileToServe}`, request.url), request);
+    const assetRequest = new Request(`https://static.kremer.men/${fileToServe}`, request);
 
     const assetResponse = await env.ASSETS.fetch(assetRequest);
 
-    // Return 404 if route wasn't matched
     if (!routes[path] && assetResponse.ok) {
       return new Response(await assetResponse.text(), {
         status: 404,
